44ce4db6abac2a0682048a60863e21e2e3d85d05
brian suda <brian@suda.co.uk>
2012-07-06 12:30:04 +0000

    Archive fected on \c

---
+++ backup.sh
@@ -0,0 +1,14 @@
+# first Git Pull, just in case multiple services are archiving
+git pull
+
+# Curl the existing site to get new content
+#wget -rkp -l2 -np -nH --cut-dirs=1 http://optional.is/required/
+
+# Git add any new files
+git add .
+
+# Git Commit the changes
+git commit -m "Archive fected on \c"; date
+
+# Git Push to keep the repo up to date
+git push
